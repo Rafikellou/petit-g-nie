8f919491a77811d9724f9da3b656a245f27741b7
Rafik KELLOU <rafik.kellou@septeo.com>
2025-01-04 21:14:39 +0100

    fix: Correction de la propriété audio_url dans le composant Listen

--- src/app/listen/page.tsx
+++ src/app/listen/page.tsx
@@ -112,7 +112,7 @@ export default function Listen() {
             </div>
 
             <AudioPlayer
-              src={selectedBook.audioUrl}
+              src={selectedBook.audio_url}
               onProgress={handleProgress}
               onPlayPause={togglePlayPause}
             />
